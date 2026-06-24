775ac15eeec0b6eab33d15f200da9c1195aa2de2
faraz <farazpourebrahimh@gmail.com>
2026-06-24 05:09:17 +0330

    feat(React): Expose Utilities, Provider And Icons From The Package Entry

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,39 @@
+// Utilities
 export { cn } from "./utils/cn.js";
+export {
+  formatDate,
+  formatNumber,
+  formatRelativeTime,
+  toLatinDigits,
+  toPersianDigits,
+  type CalendarSystem,
+  type DatePreset,
+  type DigitSystem,
+  type FormatDateOptions,
+  type FormatNumberOptions,
+  type FormatRelativeTimeOptions,
+} from "./utils/format.js";
+
+// Internationalisation
+export {
+  AveroProvider,
+  resolveAveroSettings,
+  useAvero,
+  useAveroFormatter,
+  type AveroFormatter,
+  type AveroProviderProps,
+  type AveroSettings,
+  type TextDirection,
+} from "./i18n/AveroProvider.js";
+export {
+  dictionaries,
+  en,
+  fa,
+  getDictionary,
+  type AveroDictionary,
+  type AveroLanguage,
+} from "./i18n/dictionaries.js";
+
+// Icons
+export { createIcon, type IconDefinition, type IconProps } from "./icons/createIcon.js";
+export * from "./icons/referenceIcons.generated.js";
